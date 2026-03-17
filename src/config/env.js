78a7c0a21c9fef0c3ad0cb7e42706d512faf4b04
Nodaco35 <ngodoancuong699668@gmail.com>
2026-03-18 01:30:38 +0700

    Chore: Change api base

--- src/config/env.js
+++ src/config/env.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL =
-  process.env.EXPO_PUBLIC_API_BASE_URL || "http://localhost:3000";
-
+  process.env.EXPO_PUBLIC_API_BASE_URL ||
+  "https://mma302-library-book-be.onrender.com";
